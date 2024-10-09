34b0ed3dc795cc728cf9b8ba7244407e2387613c
ehu <ehu@U032842>
2024-10-09 13:16:11 +0200

    Aldaketak

---
+++ lsk/Project.java
@@ -0,0 +1,17 @@
+package lsk;
+import java.util.Vector;
+
+public class Project {
+	public Vector<ProjectFile> files=new Vector<ProjectFile>();
+	public void addProject(ProjectFile p){
+		files.add(p);
+	}
+	public void loadAllFiles(){
+		for (ProjectFile f:files)
+			f.loadFile(f.filePath);
+	}
+	public void storeAllFiles(){
+		for (ProjectFile f:files)
+			f.storeFile();
+	}
+}
---
+++ lsk/WriteOnlyProjectFile.java
@@ -0,0 +1,9 @@
+package lsk;
+
+public class WriteOnlyProjectFile extends ProjectFile implements Storable{
+
+	public WriteOnlyProjectFile(String filePath) {
+		super(filePath);
+	}
+
+}
